8bef5a06d3f5fdce9d3ad55b08823981aa13e0c2
Ryan Andrade <andmryan@gmail.com>
2025-04-14 21:12:18 -0400

    Second lecture complete.

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const morgan = require("morgan");
 const session = require("express-session");
 
 const authController = require("./controllers/auth.js");
-const isSignedIn = require('./middleware/is-signed-in.js');
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
+const applicationsController = require('./controllers/applications.js');
 
 const port = process.env.PORT ? process.env.PORT : "3000";
 
@@ -31,14 +33,20 @@ app.use(
 
 app.use(passUserToView);
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    user: req.session.user,
-  });
+app.get('/', (req, res) => {
+  // Check if the user is signed in
+  if (req.session.user) {
+    // Redirect signed-in users to their applications index
+    res.redirect(`/users/${req.session.user._id}/applications`);
+  } else {
+    // Show the homepage for users who are not signed in
+    res.render('index.ejs');
+  }
 });
 
 app.use("/auth", authController);
 app.use(isSignedIn);
+app.use('/users/:userId/applications', applicationsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
